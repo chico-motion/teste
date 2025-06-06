e97e05f0d39e268f0f6368ede01f819d189e9a38
chico-motion <francisco.maemd@gmail.com>
2025-06-06 11:03:41 -0300

    Update vite.config.js
    added
    
    base: '/portfolio/'

--- vite.config.js
+++ vite.config.js
@@ -190,6 +190,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
+	base: '/teste/'
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin()] : []),
